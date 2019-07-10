ca5b32fd75fa9559c8492315bea418a9fafa9461
Mateti <kmateti@dxc.com>
2019-07-10 16:45:22 +0530

    other files

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 $(document).ready(function() {
     
-    // 1. data has to fetch from https://github.com/uidreams/makeOrderApp/blob/master/beverageData.json
-    // 2. on form submit show order details to 'ordered' queue   
-    $('#orderSubmit').click(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.myjson.com/bins/15ha9v',
+        contentType: 'application/x-www-form-urlencoded;charset=utf-8',
+        async: false,
+        dataType: 'json',
+        timeout: (20000),
+        cache: false,
+        crossOrigin: true,
+        success: function(data) {
+            
+            $('#beverageList').empty().append($('<option>', {
+                value: 'NONE',
+                text : 'Please Select'
+            }));
+            $.each(data, function (val) {
+                $('#beverageList').append($('<option>', {
+                    value: val.Description,
+                    text: val.Description,
+                }));
+            });
+        },
+        error: function() {
+            alert('Beverage list not loaded.')
+        }
+    })
+    // 2. based on user(admin / user) profile selected, give right to change status
+
+    $('#orderSubmit').on('click', function() {
         var username = $('#username').val();
         var beverage = $('#beverageList :selected').text();
-        var orderDetails = '<ul><li>' + username + '</li><li>' + beverage + '</li> </ul>';
+        var orderDetails = '<ul id="orderInfo"><li>' + username + '</li><li>' + beverage + '</li> </ul>';
         $('#queue').append(orderDetails);
-    });     
-    // 3. after 30 sec. show order details to 'processing' queue
-    // 4. after 30 sec. show order details to 'Ready to collect' queue
-
-    // write settimeout function for automatic status update after specified time.
-    
-    //Instead, give an option to move to another queue on clicking move icon of order
-    
-    // for(let i = 0; i < $('#queue ul').length; i++) {
-    //     this.attr('id', $('#queue') + i);
-    // }
-    if($('#queue ul').length > 1) {
-        $('#queue ul li').click(function() {
-            alert('hii');
-        });
-    }
+    });
+    // on clicking 
+    $('#queue').on('click', 'ul', function() {
+        var queueOrder = $(this).clone();
+        $(this).remove();
+        queueOrder.appendTo($('#processing'));
+    });
+    $('#processing').on('click', 'ul', function() {
+        var processingOrder = $(this).clone();
+        $(this).remove();
+        processingOrder.appendTo($('#ready'));
+    });
 
 
 })
